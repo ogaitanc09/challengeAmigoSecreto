b5b16384b607405ae59c2de7a6ac55254c0dc1a0
OscarGaitan <ogaitanc09@gmail.com>
2025-02-18 15:20:26 -0500

    Actualizacion agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    if (nombre.trim() === ''){
+        alert('Por favor, inserte su nombre');
+    } else {
+        amigos.push(nombre);
+        //console.log(amigos);
+        document.getElementById('amigo').value = '';
+    }
+    return;
+}
+
+agregarAmigo();
